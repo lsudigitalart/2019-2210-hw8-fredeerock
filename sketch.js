e51ec15f47284c7ded76be6959c58384a20c32b8
Derick Ostrenko <dostrenko@gmail.com>
2019-10-24 13:24:57 -0500

    arrays

--- sketch.js
+++ sketch.js
@@ -1,23 +1,24 @@
-var legs1, legs2, legs3, legs4;
-var eyes1, eyes2, eyes3, eyes4;
+var show;
 
-var show = 0;
 var legChoice, eyeChoice;
 
-function preload() {
-  legs1 = loadImage("legs/1.png");
-  legs2 = loadImage("legs/2.png");
-  legs3 = loadImage("legs/3.png");
-  legs4 = loadImage("legs/4.png");
+var legs = [];
+var eyes = [];
 
-  eyes1 = loadImage("eyes/1.png");
-  eyes2 = loadImage("eyes/2.png");
-  eyes3 = loadImage("eyes/3.png");
-  eyes4 = loadImage("eyes/4.png");
+function preload() {
+
+  for (var l = 0; l < 4; l++) {
+    legs[l] = loadImage("legs/" + l + ".png");
+  }
+
+  for (var e = 0; e < 4; e++) {
+    eyes[e] = loadImage("eyes/" + e + ".png")
+  }
 }
 
 function setup() {
   createCanvas(800, 600);
+  imageMode(CENTER);
 
 }
 
@@ -25,54 +26,18 @@ function draw() {
   background(100);
 
   if (show == 1) {
-    creature(100, 50, legChoice, eyeChoice);
+    creature(eyeChoice, legChoice);
   }
 
 }
 
 function mouseReleased() {
-  legChoice = int(random(4));
   eyeChoice = int(random(4));
+  legChoice = int(random(4));
   show = 1;
 }
 
-function creature(size, head, leg, eye) {
-  print(leg);
-
-  noStroke();
-  circle(width / 2, height / 2, size);
-  circle(width / 2, (height / 2) - 50, head);
-
-  if (leg == 0) {
-    image(legs1, (width / 2) - 60, (height / 2) + 20);
-  }
-
-  if (leg == 1) {
-    image(legs2, (width / 2) - 60, (height / 2) + 20);
-  }
-
-  if (leg == 2) {
-    image(legs3, (width / 2) - 60, (height / 2) + 20);
-  }
-
-  if (leg == 3) {
-    image(legs4, (width / 2) - 60, (height / 2) + 20);
-  }
-
-  if (eye == 0) {
-    image(eyes1, (width / 2) - 150, (height / 2) - 150);
-  }
-
-  if (eye == 1) {
-    image(eyes2, (width / 2) - 150, (height / 2) - 150);
-  }
-
-  if (eye == 2) {
-    image(eyes3, (width / 2) - 150, (height / 2) - 150);
-  }
-
-  if (eye == 3) {
-    image(eyes4, (width / 2) - 150, (height / 2) - 150);
-  }
-
+function creature(eye, leg) {
+  image(legs[leg], width / 2, height / 2);
+  image(eyes[eye], width / 2, (height / 2) - 100);
 }
